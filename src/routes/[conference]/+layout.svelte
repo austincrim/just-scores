7b9cabb9875b10d9f0ac7859286141ef9a054a1a
Austin Crim <aust.crim@gmail.com>
2023-03-05 18:54:42 -0600

    tweak styling

--- src/routes/[conference]/+layout.svelte
+++ src/routes/[conference]/+layout.svelte
@@ -8,26 +8,32 @@
   let categories = allConferences.splice(0, 2)
 </script>
 
-<div class="flex gap-16 relative">
-  {#each [categories, allConferences] as filter, i}
-    <div class="flex flex-col gap-3 py-4 h-36 overflow-y-auto">
-      {#each filter as conference}
-        <a
-          data-sveltekit-preload-data
-          class:selected={$page.params.conference === conference}
-          href="/{conference.trim()}"
-        >
-          {conference}
-        </a>
-      {/each}
-    </div>
-    {#if i === 1}
-      <div class="h-4 absolute -top-2 right-0 w-1/2 bg-gray-50 blur-sm" />
-      <div class="h-4 absolute -bottom-2 right-0 w-1/2 bg-gray-50 blur-sm" />
-    {/if}
-  {/each}
+<div class="w-full flex flex-col items-center md:flex-row md:items-start">
+  <div class="flex gap-16 relative px-12">
+    {#each [categories, allConferences] as filter, i}
+      <div
+        class="flex flex-col gap-3 py-4 h-48 md:h-[70vh] overflow-y-auto px-3 rounded-md"
+        class:border={i === 1}
+        class:shadow-sm={i === 1}
+      >
+        {#each filter as conference}
+          <a
+            data-sveltekit-preload-data
+            class:selected={$page.params.conference === conference}
+            href="/{conference.trim()}"
+          >
+            {conference}
+          </a>
+        {/each}
+      </div>
+      {#if i === 1}
+        <div class="h-4 absolute -top-2 right-0 w-1/2 bg-gray-50 blur-sm" />
+        <div class="h-4 absolute -bottom-2 right-0 w-1/2 bg-gray-50 blur-sm" />
+      {/if}
+    {/each}
+  </div>
+  <slot />
 </div>
-<slot />
 
 <style>
   .selected {
